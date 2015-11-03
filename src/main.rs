c545b9ab7bf64684e46f8cff9065bec6f3f22101
Saghm Rossi <saghmrossi@gmail.com>
2015-11-03 13:17:46 -0500

    add prompts/labels to interpreter for readability

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,28 @@ extern crate jsrs_parser;
 
 mod eval;
 
-use std::io;
+use std::io::{self, Write};
 use std::collections::HashMap;
 use eval::eval_string;
 
 fn main() {
     let mut state = HashMap::new();
+    let stdin = io::stdin();
+    let mut stdout = io::stdout();
 
     loop {
+        // prompt
+        print!(">> ");
+        stdout.flush().unwrap();
+
         // read input
         let mut input = String::new();
-        io::stdin().read_line(&mut input).unwrap();
+        stdin.read_line(&mut input).unwrap();
         if input.len() == 0 {
             break;
         }
 
-        println!("{:?}", eval_string(&input, &mut state));
-        println!("{:?}", state);
+        println!("=> {:?}", eval_string(&input, &mut state));
+        println!("** {:?}", state);
     }
 }
