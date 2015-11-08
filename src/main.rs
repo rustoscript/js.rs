1a63e624d57f1d04c6ea928df6206dcb455d4626
Terry Sun <terrynsun@gmail.com>
2015-11-08 12:15:40 -0500

    Exit cleanly on ^D

--- src/main.rs
+++ src/main.rs
@@ -23,16 +23,25 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-        if input == "\n" {
-            continue;
+        // exit if no input (eg. ^D)
+        if input.len() == 0 {
+            println!("");
+            break;
         }
 
         input = String::from(input.trim());
 
+        // ignore if only whitespace
+        if input.len() == 0 {
+            continue;
+        }
+
+        // insert semicolon if necessary
         if !input.ends_with(";") && !input.ends_with("}") {
             input.push_str(";");
         }
 
+        // eval
         println!("=> {:?}", eval_string(&input, &mut state));
         println!("** {:?}", state);
     }
