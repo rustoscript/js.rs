5c86d9b10225bda4df7310170c2d4fd27e77db64
Terry Sun <terrynsun@gmail.com>
2015-11-10 18:20:56 -0500

    Read from file functionality.

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,48 @@ extern crate jsrs_parser;
 mod value;
 mod eval;
 
-use std::io::{self, Write};
+use std::env;
+use std::io;
+//use std::io::{self, Write};
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::path::Path;
+use std::fs::File;
+
 use std::collections::HashMap;
+
 use eval::eval_string;
 
-fn main() {
+
+fn eval_file(filename: String, debug: bool) {
+    let mut state = HashMap::new();
+
+    println!("Reading from \"{}\"", filename);
+    let path = Path::new(&filename);
+    let file = File::open(&path)
+        .expect(&format!("Cannot open \"{}\": no such file or directory", filename));
+    let file_buffer = BufReader::new(file);
+
+    for line in file_buffer.lines() {
+        let mut input = String::from(line.unwrap().trim());
+
+        if debug {
+            println!(">> {:?}", input);
+        }
+
+        // insert semicolon if necessary
+        if !input.ends_with(";") && !input.ends_with("}") {
+            input.push_str(";");
+        }
+
+        if debug {
+            println!("=> {:?}", eval_string(&input, &mut state));
+            println!("** {:?}", state);
+        }
+    }
+}
+
+fn repl() {
     let mut state = HashMap::new();
     let stdin = io::stdin();
     let mut stdout = io::stdout();
@@ -42,5 +79,16 @@ fn main() {
         // eval
         println!("=> {:?}", eval_string(&input, &mut state));
         println!("** {:?}", state);
+    }
+}
+
+fn main() {
+    let args = env::args();
+    if args.len() > 1 {
+        for file in args.skip(1) {
+            eval_file(file, true);
+        }
+    } else {
+        repl();
     }
 }
