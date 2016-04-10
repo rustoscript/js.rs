b3a54ae9b4b7fa8739470d5dbe75086f3e56a126
Saghm Rossi <saghmrossi@gmail.com>
2016-04-10 00:17:29 -0400

    make `scalar` inline

--- src/var.rs
+++ src/var.rs
@@ -3,6 +3,7 @@ use jsrs_common::types::js_var::{JsVar, JsPtrEnum, JsType};
 pub type JsVarValue = (JsVar, Option<JsPtrEnum>);
 pub type JsReturnValue = Option<JsVarValue>;
 
+#[inline]
 // Helper to avoid repeating this everywhere
 pub fn scalar(v: JsType) -> (JsVar, Option<JsPtrEnum>) {
     (JsVar::new(v), None)
