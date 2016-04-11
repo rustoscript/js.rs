7d8851c07633595a43e8bcd0aa75358a2b3ba24c
Terry Sun <terrynsun@gmail.com>
2016-04-11 18:25:08 -0400

    Add mod, exp, shifts, bitwise operators

--- parse.py
+++ parse.py
@@ -1,14 +1,18 @@
 import re
 from collections import defaultdict
 
-reg = re.compile("(.*): (\w+)Error")
+reg_err = re.compile("(.*): (\w+)Error")
+reg_ok = re.compile("(.*): OK")
+
 f = open('test_results.txt')
 counts = defaultdict(lambda: 0)
 for line in f:
     #print(line)
-    result = reg.search(line)
+    result = reg_err.search(line)
     if result is not None:
-        #print(result.group(1), result.group(2))
         counts[result.group(2)] += 1
+    result = reg_ok.search(line)
+    if result is not None:
+        counts["OK"] += 1
 
 print(counts)
