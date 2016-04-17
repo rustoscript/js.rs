34038d503daca9ab1b0d2305f373c728f6ba545d
Terry Sun <terrynsun@gmail.com>
2016-04-17 15:52:58 -0400

    Compute EqlStrict and NeqStrict

--- src/number.rs
+++ src/number.rs
@@ -8,6 +8,7 @@ use jsrs_common::ast::*;
 use jsrs_common::ast::BinOp::*;
 use jsrs_common::types::coerce::{AsBool,AsNumber};
 use jsrs_common::types::js_var::{JsVar, JsType};
+use jsrs_common::types::js_var::JsPtrEnum::*;
 use jsrs_common::types::js_var::JsType::*;
 use jsrs_common::js_error::{self, JsError};
 
@@ -36,22 +37,33 @@ pub fn eval_binop(op: &BinOp, val1: JsVar, val2: JsVar,
         Eql => JsBool(b!(val1) == b!(val2)),
 
         EqlStrict => {
-            let val1_ptr = match state.borrow_mut().load(&val1.binding) {
-                Ok((_, ptr)) => ptr,
-                Err(e) => {
-                    return Err(JsError::ReferenceError(format!("{:?} is not defined",
-                                                               val1.binding)));
+            let b = match (&val1.t, &val2.t) {
+                (&JsNull,      &JsNull) => true,
+                (&JsUndef,     &JsUndef) => true,
+                (&JsNum(ref n1),   &JsNum(ref n2)) => n1 == n2,
+                (&JsBool(ref b1),  &JsBool(ref b2)) => b1 == b2,
+                (&JsPtr(_), &JsPtr(_)) => {
+                    let ptr1 = try_load!(state, &val1.binding);
+                    let ptr2 = try_load!(state, &val2.binding);
+                    match (&ptr1, &ptr2) {
+                        (&Some(JsSym(_)),      &Some(JsSym(_))) => val1 == val2,
+                        (&Some(JsStr(ref s1)), &Some(JsStr(ref s2))) => s1 == s2,
+                        (&Some(JsObj(_)),      &Some(JsObj(_))) => val1 == val2,
+                        (&Some(JsFn(_)),       &Some(JsFn(_))) => val1 == val2,
+                        _ => false,
+                    }
                 }
+                _ => false,
             };
-            let val2_ptr = match state.borrow_mut().load(&val2.binding) {
-                Ok((_, ptr)) => ptr,
-                Err(_) => return Err(JsError::ReferenceError(format!("{:?} is not defined",
-                                                                     val2.binding))),
-            };
-            //println!("{:#?} {:#?}", val1_ptr, val2_ptr);
-            JsBool(val1.t == val2.t)
+            JsBool(b)
         }
-        NeqStrict => JsBool(val1.t != val2.t),
+        NeqStrict => {
+            if let Ok(JsBool(b)) = eval_binop(&EqlStrict, val1, val2, state) {
+                JsBool(!b)
+            } else {
+                JsBool(false)
+            }
+        }
 
         BitOr  => JsNum((ni64!(val1) | ni64!(val2)) as f64),
         BitXor => JsNum((ni64!(val1) ^ ni64!(val2)) as f64),
