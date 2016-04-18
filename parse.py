8d7e2a33b71e6e11e6a8d581226b4cbb18054c28
Saghm Rossi <saghmrossi@gmail.com>
2016-04-17 23:10:36 -0400

    get stats for categories

--- parse.py
+++ parse.py
@@ -2,25 +2,32 @@
 import re
 import operator
 from collections import defaultdict
+import sys
 
 COMMON_ERRS_ONLY = True
 
-reg_err = re.compile("(.*): (\w+)Error: (.*)")
-reg_ok = re.compile("(.*): OK")
+reg_err = re.compile("sputnik/([\w\.]+/)*([\w\.]+)/.*: (\w+)Error: (.*)")
+reg_ok = re.compile("sputnik/([\w\.]+/)*([\w\.]+)/.*: OK")
 
 f = open('test_results.txt')
 counts = defaultdict(lambda: defaultdict(lambda: 0))
 total = 0
+all_groups = set()
 
 for line in f:
     result = reg_err.search(line)
     if result is not None:
-        counts[result.group(2)][result.group(3)] += 1
+        counts[result.group(3)][result.group(4)] += 1
+        all_groups.add(result.group(2))
         continue
 
     result = reg_ok.search(line)
     if result is not None:
-        counts["OK"][""] += 1
+        all_groups.add(result.group(2))
+        counts["OK"][result.group(2)] += 1
+        continue
+
+    sys.stderr.write(line)
 
 #for k in sorted(counts.items(), key=operator.itemgetter(1)):
 for k in counts.keys():
@@ -40,5 +47,6 @@ for k in counts.keys():
     print("{}:\t{} groups,\t{} total".format(k[:6], len(counts[k]),
         sum(v for k, v in counts[k].items())))
 
+print("Number of categories with at least some passes: %s/%s" % (len(counts["OK"]), len(all_groups)))
 ok = sum(v for k, v in counts["OK"].items())
 print("Passed: {}/{} = {}%".format(ok, total, ok/total * 100))
